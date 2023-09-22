ef3119b11c1a31d809067ac591a873aee9d12362
psantella0908 <145798356+psantella0908@users.noreply.github.com>
2023-09-22 19:39:42 +0200

    Create testprova.py
    Estensione del codice iniziale con aggiunta di ciclo

---
+++ testprova.py
@@ -0,0 +1,36 @@
+import openai
+openai.api_key = "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"
+input_text = "start"
+system_message = """."""
+
+
+def ask_gpt(messages):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.2,
+        max_tokens=2000
+    )
+    return completion.choices[0].message["content"]
+
+
+print("Questo è un bot progettato per assistere gli studenti affetti da DSA. Per terminare inserire 'END'.")
+history = []
+messages = [{"role": "system", "content": system_message}]
+while input_text != "END":
+    input_text = input("Inserisci la domanda: ")
+    if input_text == "END":
+        print("Conversazione terminata. \n")
+        break
+    messages.append({"role": "user", "content": input_text})
+    history.append("Utente: " + input_text)
+    response = ask_gpt(messages)
+    messages.append({"role": "assistant", "content": response})
+    history.append("Assistente: " + response)
+    print("\nAssistente : ", response)
+    print("\n")
+
+print("Ecco il riassunto della conversazione: ")
+for s in history:
+    print(s)
+    print("\n")
